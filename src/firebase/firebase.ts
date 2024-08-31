a5fb63fd85da5a3a942b33c7ce0017b1e404d2d4
ABEL OTEGBOLA <abel15655@gmail.com>
2024-08-31 20:28:37 +0100

    fix: remove key and appid from env

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+    apiKey: "AIzaSyDj2-YvUs4-VdgYPzkTp7FHjQ8H0eLMp6Y",
     authDomain: "devlinks-hng-8.firebaseapp.com",
     projectId: "devlinks-hng-8",
     storageBucket: "devlinks-hng-8.appspot.com",
     messagingSenderId: "290009761564",
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+    appId: "1:290009761564:web:3e3b2fea98e0ca622a9cd9",
     measurementId: "G-QWMRE0DQG8"
 };
 
