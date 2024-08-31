9cb8b43e8ea51b8521713b2d5ee494807fd5813a
ABEL OTEGBOLA <abel15655@gmail.com>
2024-08-31 21:00:49 +0100

    style: add opacity to home navs

--- src/components/tab/tab.tsx
+++ src/components/tab/tab.tsx
@@ -16,12 +16,12 @@ export default function Tab ({ to, label, icon }: tabProps) {
     return (
         <Link
             href={to}
-            className={`flex items-center justify-center md:flex-row flex-col gap-1 h-[32px] p-[8px_16px] hover:text-primary font-semibold rounded-[4px]
+            className={`flex items-center justify-center md:flex-row flex-col md:gap-1 gap-2 h-[32px] p-[8px_16px] hover:text-primary font-semibold rounded-[4px]
                 ${pathname === to ? "bg-tetiary text-primary" : ""}
             `}
         >
             <span className="md:text-lg text-2xl opacity-[0.6]">{icon}</span>
-            <span className="md:inline md:text-[12px] text-[8px]">{label}</span>
+            <span className="md:inline md:text-[12px] opacity-[0.6] text-[8px]">{label}</span>
         </Link>
     )
 }
