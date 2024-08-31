7a0d299dd499d2474591bb9d715dd0531f7c93c1
ABEL OTEGBOLA <abel15655@gmail.com>
2024-08-31 20:52:52 +0100

    feat: add hero to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Header from "@/components/header/header";
+import Button from "@/components/button/button";
 import Image from "next/image";
 
 export default function Homepage() {
 
     return (
       <main>
+        <div className="md:px-[8%] md:py-[3%] p-8 flex justify-between bg-tetiary gap-5 lg:flex-nowrap flex-wrap">
+          
+          <div className="flex flex-col gap-7 lg:w-[50%] mt-[5%]">
+              <h1 className="xl:text-[58px] sm:text-[48px] text-[32px] font-bold leading-[120%]">Discover Musical Talents, Lessons, Gigs and Gears</h1>
+              <p className="mb-2">Bri8 musicals find talented musicians for your next gig, learn from our expert instructors, buy, sell or rent musical instruments .</p>
+              <div className="flex gap-4">
+                <Button href="/shop">Explore Products</Button>
+                <Button href="/talents" variant="secondary">Discover Talents</Button>
+              </div>
+          </div>
+            
+          <Image src="/hero-image.png" width={500} height={500} alt="girl listening to music" className="lg:block" />
+
+        </div>
       </main>
     )
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
     ]
 
     return (
-        <div className="flex items-center justify-between lg:sticky top-0 left-0 w-full lg:px-[5%] px-8 bg-white p-3 rounded-[8px] border border-transparent border-b-primary/[0.1]">
+        <div className="flex items-center justify-between lg:sticky top-0 left-0 w-full lg:px-[8%] px-8 bg-white p-3 rounded-[8px] border border-transparent border-b-primary/[0.1]">
             <Image src="/logo.svg" width={30} height={30} alt="logo" className="lg:block" />
 
             <nav className="flex items-center justify-between gap-4 bg-white lg:w-auto w-full lg:static fixed left-0 bottom-0 z-[50] lg:p-0 p-4 lg:border-none border border-transparent border-t-primary/[0.2]">
@@ -32,7 +32,7 @@ export default function Header() {
                 }
             </nav>
 
-            <div className="md:flex items-center gap-6 w-[40%] hidden">
+            <div className="md:flex items-center gap-6 xl:w-[40%] md:w-[35%] hidden">
                 <Search placeholder="Search Products, Gigs and Talents" />
                 <Button href={"/login"} >Login</Button>
             </div>
