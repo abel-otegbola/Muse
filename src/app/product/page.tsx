8d00a851a4331fd369f4a9880a6d791021d54f9f
ABEL OTEGBOLA <abel15655@gmail.com>
2024-09-01 18:04:21 +0100

    feat: create product page, edit shop page

---
+++ src/app/product/page.tsx
@@ -0,0 +1,143 @@
+'use client'
+import Button from "@/components/button/button";
+import { storeContext } from "@/context/useStore";
+import { MusicInstruments } from "@/data/musicInstruments";
+import { currencyFormatter } from "@/helpers/currencyConverter";
+import { IProductProps } from "@/interface/store";
+import { Heart, Minus, Plus } from "@phosphor-icons/react";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+import { LoaderIcon } from "react-hot-toast";
+
+export default function ProductPage () {
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id") || 0
+    const { cart, setCart, wishlist, setWishlist } = useContext(storeContext)
+    const [product, setProduct] = useState<IProductProps>({ id: "0",  title: "", description: "", categories: [],tags: [], img: "", price: 0 })
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setProduct(MusicInstruments.filter(item => item.id === id)[0])
+    }, [id])
+
+    const addToCart = (id: number | string | null) => {
+        setCart([ {id, quantity: 1}, ...cart ])
+    }
+
+    const removeFromCart = (id: number | string | null) => {
+        setCart(cart.filter((item: any) => item.id !== id))
+    }
+
+    const changeQuantity = (id: number | string | null, action: string) => {
+        let newList = cart.map((item: any) => {
+            if(item.id === id) {
+                if(action === "minus" && item.quantity > 1) {
+                    item.quantity -= 1
+                }
+                else if(action === "plus") {
+                    item.quantity += 1
+                }
+                return item
+            }
+            else return item;
+        })
+        setCart(newList)
+    }
+
+    const addToWishlist = (id: number | string | null) => {
+        setWishlist([ id, ...wishlist ])
+    }
+
+    const removeFromWishlist = (id: number | string | null) => {
+        setWishlist(wishlist.filter((item: any) => item !== id))
+    }
+
+
+    return (<div className="md:p-[8%] p-[3%]">
+        {
+            loading ? <LoaderIcon /> :
+                <div key={id}>
+                    <div className="relative flex flex-wrap justify-center items-center my-2 rounded gap-[5%]">
+                        <div className="md:w-[400px] w-full h-[472px] relative bg-gray/[0.3]">
+                        {/* <Slide arrows={false} indicators={true} cssClass="" easing="linear" transitionDuration={500} duration={3000}>
+                            {
+                                [product.thumbnail, ...product.images].map((img: any, i: number) => (
+                                    <div className={`each-slide-effect`} key={i}>
+                                        <img src={img.url} width={"100%"}  />
+                                    </div>
+                                ))
+                            }
+                        </Slide> */}
+                        
+                            <Image src={"/" +product.img} alt={product.title} fill sizes="100%" className="bg-cover" />
+                        </div>
+                        <div className="md:px-[3%] px-4 md:py-0 py-6 md:w-[50%] w-full">
+                            <h2 className="py-2 md:text-[40px] text-[28px] font-bold">{product?.title}</h2>
+
+                            
+                            <div className="flex items-center justify-between gap-2">
+                                <p className="flex items-center text-[20px] font-bold py-4">{currencyFormatter(product?.price)}</p>
+                                <div>
+                                {
+                                    wishlist.indexOf(id) === -1 ? 
+                                    <button className="h-[40px] text-gray-300 flex items-center gap-2" onClick={() => addToWishlist(id) }><Heart size={16}/>Save to wishlist</button> 
+                                    : 
+                                    <button className=" h-[40px] text-red-500 flex items-center gap-2" onClick={() => removeFromWishlist(id)}><Heart size={16}/> Remove from wishlist</button> 
+                                }
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-2 py-4">
+                                <p className="rounded py-1 w-fit"><span className="font-bold">Categories:</span> {product?.categories + ", "}</p>
+                                <p className="rounded py-1 w-fit"><span className="font-bold">Tags:</span> {product?.tags + ", "}</p>
+                            </div>                            
+
+                            <div className="mt-4 px-1">
+                                <div className="my-2">
+                                    <p className="font-bold">Description: </p>
+                                    <div className="mt-4">
+                                        <div dangerouslySetInnerHTML={{ __html: product?.description}} className="w-full"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="mt-6 flex items-center gap-4 py-4">
+                                
+                                <div className="text-[14px] border border-transparent border-t-black/[0.1] sm:px-0 py-6 w-full sm:z-0 bg-white">
+                                {
+                                    cart.map((item: any) => item.id).indexOf(id) === -1 ? 
+                                    <Button size="full" onClick={() => addToCart(id) } >Add to Cart</Button>
+                                    : 
+                                    <div className="flex gap-2">
+                                        <Button size="full" variant="secondary" onClick={() => removeFromCart(id)} >Remove From Cart</Button>
+                                        <div className="flex items-center gap-1 animate-zoom-in ">
+                                            <button className="h-[40px] p-[12px]" onClick={() => changeQuantity(id, "minus")}><Minus /></button>
+                                            <p className="flex items-center justify-center h-[40px] p-[12px] py-1 rounded bg-transparent w-[70px] border border-gray/[0.5]">{cart.filter((item: any) => item.id === id).map((item: any) => item.quantity)}</p>
+                                            <button className="h-[40px] p-[12px]" onClick={() => changeQuantity(id, "plus")}><Plus /></button>
+                                        </div> 
+                                    </div>
+                                    
+                                }
+                                </div>
+
+                               
+                            </div>
+                        </div>
+                    </div> 
+                    <h2 className="border border-transparent border-b-gray-200 dark:border-b-gray-100/[0.1] mt-20 text-purple text-semibold uppercase">Related Products</h2>
+                    <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 sm:gap-4 gap-2 py-[40px] ">
+                        {/* {
+                            MusicInstruments.filter((item: any) => item.category === product.category && item.id !== id).slice(0,5).map((product: any) => (
+                            <ProductCard key={product.id} product={product.data} />
+                        ))
+                        } */}
+                    </div>
+                </div>
+                
+        }
+
+        </div>
+    )
+
+}
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,41 +1,46 @@
 'use client'
 import Button from "@/components/button/button";
 import ProductCard from "@/components/cards/productCard";
+import Dropdown from "@/components/dropdown/dropdown";
 import Search from "@/components/search/search";
-import { ArrowRight } from "@phosphor-icons/react";
-import Image from "next/image";
-import Link from "next/link";
+import { MusicInstruments } from "@/data/musicInstruments";
+import { Funnel, SortAscending, SortDescending } from "@phosphor-icons/react";
+import { useState } from "react";
 
-export default function Homepage() {
+export default function Shoppage() {
+  const [sort, setSort] = useState("Latest")
+
+  const sortOptions = [
+    { id: 0, title: "Latest", icon: <SortAscending /> },
+    { id: 1, title: "Oldest", icon: <SortDescending /> },
+  ]
 
     return (
       <main>
         <div className="md:px-[8%] md:py-[3%] p-8 flex flex-col items-center justify-start bg-tetiary gap-5 lg:flex-nowrap flex-wrap">
           
-          <div className="flex flex-col gap-3 items-center mt-[5%] md:w-[80%]">
+          <div className="flex flex-col gap-3 items-center my-[5%] md:w-[80%]">
               <h1 className="xl:text-[40px] text-center text-[32px] font-bold leading-[120%]">Find Quality Musical Instruments </h1>
               <p className="mb-2 text-center">Bri8 musicals find talented musicians for your next gig, learn from our expert instructors, buy, sell or rent musical instruments .</p>
               <div className="flex gap-2">
                 <Search placeholder="Search products" />
-                <Button>Search</Button>
+                <Button onClick={() => {}}>Search</Button>
               </div>
           </div>
-            
-            <div className="flex items-center justify-center my-12">
-                <Image src="/bass-guitar-2.png" width={450} height={200} alt="bass guitar" className="lg:block" />
-            </div>
 
         </div>
 
-        <div className="grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-6 md:px-[8%] px-8 py-[3%]">
+        <div className="flex justify-between md:px-[8%] p-8  ">
+          <div className="flex items-center gap-2"></div>
+          <div className="flex items-center gap-2">
+            <Button onClick={() => {}} variant="secondary"><Funnel size={16} /> Filter</Button>
+            <Dropdown name="Sort" onChange={setSort} value={sort} error={""} options={sortOptions} placeholder="Sort by" className="w-[100px]"/>
+          </div>
+        </div>
+
+        <div className="grid lg:grid-cols-5 sm:grid-cols-3 grid-cols-2 md:gap-6 gap-4 md:px-[8%] px-4 pb-[3%]">
           {
-            [ 
-              { id: "0", title: "Acoustic Guitar ", img: "products/acoustic.png", price: 400, categories: "", tags: [""], description: "" },
-              { id: "1", title: "Electric Guitar ", img: "products/electric.png", price: 12000, categories: "", tags: [""], description: "" },
-              { id: "2", title: "Keyboard Piano ", img: "products/piano.png", price: 3000, categories: "", tags: [""], description: "" },
-              { id: "4", title: "Violin ", img: "products/violin.png", price: 400, categories: "", tags: [""], description: "" },
-              { id: "3", title: "Electric Guitar Vintage ", img: "products/electric-2.png", price: 3000, categories: "", tags: [""], description: "" },
-            ]
+            MusicInstruments
             .map(item => (
                 <ProductCard key={item.id} product={item} />
             ))
