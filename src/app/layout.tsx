ab99bfd7a2f0cafe53b9c43342f7a3cb92706fcb
abel-otegbola <abel15655@gmail.com>
2026-08-12 09:01:05 +0100

    feat: add Lufga font family to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,61 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import AuthProvider from "@/context/useAuth";
 import StoreContextProvider from "@/context/useStore";
 import Footer from "@/components/footer/footer";
 import CheckDashboard from "@/components/header/checkHeader";
 
-const inter = Inter({ subsets: ["latin"] });
+const lufga = localFont({
+  src: [
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-extralight.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-semibold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-extrabold.otf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/lufga/Fontspring-DEMO-lufga-black.otf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "MUSE",
@@ -21,7 +70,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-        <body className={inter.className + " text-[12px] text-secondary dark:bg-dark dark:text-gray"}>
+        <body className={lufga.className + " text-[12px] text-secondary dark:bg-dark dark:text-gray"}>
           <AuthProvider>
             <StoreContextProvider>
               <CheckDashboard />
